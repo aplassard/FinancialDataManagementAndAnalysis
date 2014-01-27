12797227ae67ad227a4de4a85496e96092d04af7
Andrew Plassard <Andrew.Plassard@gmail.com>
2014-01-26 20:12:36 -0600

    Added .py files to download company and industry data

---
+++ lab1/scripts/download_industry_company_info.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import urllib2
+import xml.etree.ElementTree as ET
+
+url_template = '''http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.industry%20where%20id%3D%22{id}%22%3B&diagnostics=true&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'''
+prev_file = os.path.abspath('../data/industry.csv')
+out_file  = os.path.abspath('../data/company.csv')
+
+if __name__=='__main__':
+	f = open(prev_file,'r')
+	out = open(out_file,'w')
+	header = f.readline()
+	for line in f:
+		line = line.strip().split(",")
+		industry_id = line[2]
+		print line[2]
+		url = url_template.format(id=line[2])
+		u = urllib2.urlopen(url)
+		a = u.read()
+		root = ET.fromstring(a)
+		for child in root:
+			if child.tag=='results':
+				results = child
+		for child in results:
+			companies = child
+		for company in companies:
+			company_symbol = company.get('symbol')
+			company_name   = company.get('name')
+			if company_symbol.find('.')==-1:
+				try:
+					out.write('\t'.join([industry_id,company_name,company_symbol]))
+					out.write('\n')
+				except:
+					print "Couldn't write %s %s" % (company_name,company_symbol)
+	out.close()
---
+++ lab1/scripts/download_sector_industry_info.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import os
+import urllib2
+import xml.etree.ElementTree as ET
+
+url = '''http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.sectors%3B&diagnostics=false&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'''
+
+if __name__=='__main__':
+	u = urllib2.urlopen(url)
+	a = u.read()
+	root = ET.fromstring(a)
+	out_file = open(os.path.abspath('../data/industry.csv'),'w')
+	out_file.write(','.join(['Sector_Name','Industry_Name','Industry_ID']))
+	out_file.write('\n')
+	sectors = None
+	for child in root:
+		if child.tag=='results':
+			sectors = child
+	for sector in sectors:
+		sector_name = sector.get('name')
+		for industry in sector:
+			industry_name = industry.get('name')
+			industry_name = industry_name.replace(",",";")
+			industry_id   = industry.get('id')
+			industry_id = industry_id.replace(",",";")
+			out_file.write(','.join([sector_name,industry_name,industry_id]))
+			out_file.write('\n')
+	out_file.close()
