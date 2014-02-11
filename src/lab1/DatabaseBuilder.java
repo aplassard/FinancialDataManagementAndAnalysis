d753cd44e35aedc38ef0c9662b285817a3f81f57
aplassage <andrew.plassard@gmail.com>
2014-02-10 20:09:53 -0600

    Added code to load sector table data

--- src/lab1/DatabaseBuilder.java
+++ src/lab1/DatabaseBuilder.java
@@ -1,8 +1,34 @@
 package lab1;
 
+import java.io.*;
 import java.sql.*;
 
 public class DatabaseBuilder {
+	
+	public static void loadSectorTable(Connection connection,File file){
+		BufferedReader br=null;
+		try {
+			String line=null;
+			br = new BufferedReader(new FileReader(file));
+			String[] lineInfo;
+			Statement statement = connection.createStatement();
+			statement.setQueryTimeout(30);  // set timeout to 30 sec.
+			while((line=br.readLine())!=null){
+				lineInfo = line.split(",");
+				String stat = "insert into sector values("+lineInfo[0]+",\""+lineInfo[1]+"\")";
+				statement.execute(stat);
+			}
+			br.close();
+		} catch (IOException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * @param args
@@ -17,6 +43,7 @@ public class DatabaseBuilder {
 		try
 		{
 			// create a database connection
+			File f=null;
 			connection = DriverManager.getConnection("jdbc:sqlite:lab1.db");
 			System.out.println("Successfully created the database");
 			Statement statement = connection.createStatement();
@@ -39,6 +66,12 @@ public class DatabaseBuilder {
 			statement.execute("drop table if exists stock_price;");
 			statement.execute("create table stock_price(\"id\" INTEGER PRIMARY KEY NOT NULL,\"open\" REAL NOT NULL,\"close\" REAL NOT NULL,\"high\" REAL NOT NULL,\"low\" REAL NOT NULL,\"volume\" INTEGER NOT NULL,\"date\" DATETIME NOT NULL,\"company_stockID\" INTEGER NOT NULL,FOREIGN KEY(\"company_stockID\") REFERENCES company_stock(\"id\"));");
 			System.out.println("Successfully created stock_price table");
+			f = new File("lab1/data/sector.csv");
+			loadSectorTable(connection,f);
+			ResultSet rs = statement.executeQuery("select * from sector");
+			while(rs.next()){
+				System.out.println("ID: "+rs.getString("id")+" Name: "+rs.getString("name"));
+			}
 		}
 		catch(SQLException e)
 		{
